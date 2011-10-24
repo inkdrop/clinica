f1df696df847a5feeaef7f6e774f50f063cc50ad
jnwelzel <jnwelzel@gmail.com>
2011-10-24 19:14:50 -0200

    Correção alert para excluir registro; padronização formulários; seção Perguntas

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,7 +2,12 @@ class QuestionsController < ApplicationController
 
   before_filter :authenticate_user!, :except => [:show, :index]
   
-  uses_tiny_mce :only => [:new, :create, :edit, :update]
+  uses_tiny_mce :only => [:new, :create, :edit, :update],:options => {
+                          :theme => 'advanced',
+                          :theme_advanced_resizing => true,
+                          :theme_advanced_resize_horizontal => false,
+                          :plugins => %w{ table fullscreen }
+                        }
 
   # GET /questions
   # GET /questions.xml
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,6 @@
 class Question < ActiveRecord::Base
+
+  validates :question, :answer, :presence => true
+  validates :question, :length => {:minimum => 6, :maximum => 255}
+
 end
